1db7276c5a5689c387376b38b6b8962ec42188eb
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-09-27 16:07:25 +0300

    adding,updating and removing elements in arrays

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,20 +3,46 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterby, setFilterby]= useState("All")
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray =[...foods, newFood]
+    setFoods(newFoodArray)
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+  function handleRemoveFood(id){
+    const newArrayofFood = foods.filter(food => food.id !== id)
+    setFoods(newArrayofFood)
+  
+  }
+
+  function handleFilterChange(event){
+    setFilterby(event.target.value)
+  }
+
+  const foodsToDisplay = foods.filter(food => {
+    if(filterby === "All"){
+      return true;
+    }else{
+      return food.cuisine === filterby
+    }
+})
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={() => handleRemoveFood(food.id)} >  
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
 
   return (
     <div>
+      <select name="filter" onChange={handleFilterChange}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
     </div>
